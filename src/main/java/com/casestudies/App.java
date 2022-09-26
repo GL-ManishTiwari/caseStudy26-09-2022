c61750bed82bbb6b8fe9a235cbb6a1c4b2bc12d9
Manish Tiwari <manish.tiwari2@globallogic.com>
2022-09-26 15:01:38 +0530

    Case Study First Sample Commit

---
+++ src/main/java/com/casestudies/App.java
@@ -0,0 +1,13 @@
+package com.casestudies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
